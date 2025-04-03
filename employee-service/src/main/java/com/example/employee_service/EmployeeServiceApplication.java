4b21775e91bee69862901aeb6f9db94f66f613ac
ManiVassarLabs <manikanta.yasarapu@vassarlabs.com>
2025-04-04 01:27:06 +0530

    BACKEND :: Service Registry :: new eureka server to register the microservices

--- employee-service/src/main/java/com/example/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/employee_service/EmployeeServiceApplication.java
@@ -2,13 +2,11 @@ package com.example.employee_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-@EnableFeignClients
 public class EmployeeServiceApplication {
 
 //	@Bean
@@ -16,10 +14,10 @@ public class EmployeeServiceApplication {
 //		return new RestTemplate();
 //	}
 
-//	@Bean
-//	public WebClient webClient(){
-//		return WebClient.builder().build();
-//	}
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder().build();
+	}
 
 
 
--- employee-service/src/main/java/com/example/employee_service/service/APIClient.java
+++ employee-service/src/main/java/com/example/employee_service/service/APIClient.java
@@ -1,11 +1,11 @@
 package com.example.employee_service.service;
 
 import com.example.employee_service.dto.DepartmentDto;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080",value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080",value = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     @GetMapping("api/departments/get-department/{code}")
